7e100556575e39c1c6ef0cc327ff53f9720f20d9
Bosko <boskonikolic1998@gmail.com>
2024-08-13 19:45:14 +0200

    button commit

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,5 +1,4 @@
-"use client"
-
+"use client";
 import { CustomButtonProps } from "@/types"
 import Image from 'next/image'
 function CustomButton({title,containerStyles,handleClick,btnType,textStyles,rightIcon}:CustomButtonProps) {
